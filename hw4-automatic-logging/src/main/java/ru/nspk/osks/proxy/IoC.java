6c8d04aee9ab317d4a018ce817f47424fa630cf4
Кирилл Тимофеев <tk@webdl.ru>
2020-02-09 22:58:43 +0300

    HW4: Реализация ДЗ методом динамического прокси (законченная версия с использованием аннотаций)

--- hw4-automatic-logging/src/main/java/ru/nspk/osks/proxy/IoC.java
+++ hw4-automatic-logging/src/main/java/ru/nspk/osks/proxy/IoC.java
@@ -2,28 +2,63 @@ package ru.nspk.osks.proxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class IoC {
 
-    public static TestloggingInterface create() {
+    public static TestLoggingInterface getInstance() {
         InvocationHandler handler = new MyInvocationHandler(new TestLoggingImpl());
-        return (TestloggingInterface) Proxy.newProxyInstance(IoC.class.getClassLoader(),
-                new Class<?>[]{TestloggingInterface.class}, handler);
-
+        return (TestLoggingInterface) Proxy.newProxyInstance(IoC.class.getClassLoader(),
+                new Class<?>[]{TestLoggingInterface.class}, handler);
     }
 
     static class MyInvocationHandler implements InvocationHandler {
-        private final TestloggingInterface testLogging;
+        private final TestLoggingInterface testLogging;
+        private ArrayList<String> annotatedMethods;
 
-        public MyInvocationHandler(TestloggingInterface testLogging) {
+        public MyInvocationHandler(TestLoggingInterface testLogging) {
             this.testLogging = testLogging;
+            this.annotatedMethods = setAnnotatedMethods();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            System.out.println("executed method: " + method.getName() + ", param: " + args[0]);
+            if (annotatedMethods.contains(getSignature(method))) {
+                if (method.getParameterCount() == 0) {
+                    System.out.println("Executed method: " + method.getName() + ", params: Without params");
+                } else {
+                    System.out.println("Executed method: " + method.getName() + ", params: " + concatenate(args));
+                }
+            }
             return method.invoke(testLogging, args);
+        }
+
+        private static <T> String concatenate(T[] objs) {
+            return Stream.of(objs).map(Object::toString).collect(Collectors.joining(", "));
+        }
+
+        private ArrayList<String> setAnnotatedMethods() {
+            ArrayList<String> annotatedMethods = new ArrayList<>();
+            Method[] methods = TestLoggingImpl.class.getMethods();
+            for (Method method : methods) {
+                if (method.getAnnotation(Log.class) != null) {
+                    annotatedMethods.add(getSignature(method));
+                }
+            }
+            return annotatedMethods;
+        }
+
+        private static String getSignature(Method method) {
+            StringBuilder result = new StringBuilder();
+            result.append(method.getName());
+            for (Parameter param : method.getParameters()) {
+                result.append(":" + param.getType().getName());
+            }
+            return result.toString();
         }
     }
 }
--- hw4-automatic-logging/src/main/java/ru/nspk/osks/proxy/TestLoggingImpl.java
+++ hw4-automatic-logging/src/main/java/ru/nspk/osks/proxy/TestLoggingImpl.java
@@ -1,9 +1,28 @@
 package ru.nspk.osks.proxy;
 
-public class TestLoggingImpl implements TestloggingInterface {
+public class TestLoggingImpl implements TestLoggingInterface {
 
+    TestLoggingImpl() {
+    }
+
+    @Log
+    @Override
+    public void calc() {
+    }
+
+    @Log
     @Override
     public void calc(int param) {
-        System.out.println("Param: " + param);
+    }
+
+    @Log
+    @Override
+    public void calc(int param1, int param2) {
+    }
+
+    @Override
+    public void calc(int param1, int param2, int param3) {
+        System.out.println(
+                "Oops, this method don't have the @Log annotation. So, we can't show you what params it contains");
     }
 }
